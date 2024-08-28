3065de73728a390c586dee6cb8e6fd2803e82ad8
Sardo <sardo.ip@sardo.work>
2024-08-28 23:17:48 +0900

    Start create weather calculation part

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,38 @@
 extern crate dotenv;
 use dotenv::dotenv;
-use teloxide::prelude::*;
+use teloxide::{prelude::*, utils::command::BotCommands};
 
 #[tokio::main]
 async fn main() {
     dotenv().ok(); // Using dot env to load environment value from file.
     pretty_env_logger::init();
-    log::info!("Starting eorzea weatcher bot...");
+    log::info!("Starting eorzea weather watching bot...");
 
     let bot = Bot::from_env();
+    Command::repl(bot, answer).await;
+}
 
-    teloxide::repl(bot, |bot: Bot, msg: Message| async move {
-        bot.send_dice(msg.chat.id).await?;
-        Ok(())
-    })
-    .await;
+// Command List
+#[derive(BotCommands, Clone)]
+#[command(rename_rule = "lowercase", description = "These commands are supported:")]
+enum Command {
+    #[command(description = "display this text.")]
+    Help,
+    #[command(description = "Register chat ID to the database, create a new user for this bot.")]
+    Register,
+    #[command(description = "handle a username and an age.", parse_with = "split")]
+    CurrentWeather { area: String },
+}
+
+async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
+    match cmd {
+        Command::Help => bot.send_message(msg.chat.id, Command::descriptions().to_string()).await?,
+        Command::Register => {
+            bot.send_message(msg.chat.id, format!("Thank you! You are already being the user of this bot.")).await?
+        },
+        Command::CurrentWeather { area } => {
+            bot.send_message(msg.chat.id, format!("Target area: @{area} ")).await?
+        }
+    };
+    Ok(())
 }
